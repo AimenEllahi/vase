7d83d8bbba6c46cd61fecbfc79af7a9d3d06b708
hananmunir <hananmunir471@gmail.com>
2024-05-20 20:12:40 +0500

    updated text

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className='infobox__content-wrapper'>
-      Please note the following update new concerning the for student visa
-      appointments: As already announced since October 13th 2023 on this website
-      the Embassy will open the student visa registration list on November 15th
-      2023 for students holding an admission letter from a German university for
-      Bachelor or Master studies for summer semester 2024. Unfortunately, the
-      Embassy was not able to fulfil all appointment requests for winter
-      semester 2023 due to the enormous interest of potential students. This
-      demand far exceeded the capacities of the Embassy. We understand the
-      disappointment this may have caused to those applicants who could not be
-      considered. All remaining registrations of students holding an admission
-      letter from a German university for Bachelor or Master studies for winter
-      semester 2023 will be cancelled before November 15th, the candidates will
-      receive an automatically generated information by e-mail about the
-      cancellation. As of December 4th appointments for Bachelor and Master
-      students holding an unconditional direct admission letter for summer
-      semester 2024 will be entertained.
+    <div className='infobox__content-wrapper wrapper'>
+      Appointment waiting list to apply for a visa for family reunion or
+      marriage (for stays exceeding 90 days) Continue Appointment waiting list
+      to apply for a work visa (for stays exceeding 90 days) Continue
+      Appointment waiting list to apply for a work visa - Only for applicants
+      with pre-approval as per §81a Residence Act Residence Act Continue
+      Appointment waiting list for Schengen visa Continue
       <select id='appointment_newAppointmentForm_fields_3__content'>
         {/* add options */}
         <option value='1'>1</option>
