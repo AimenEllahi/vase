51ac3844482f2cbcc87addecc651c5c7484dfa6d
hananmunir <hananmunir471@gmail.com>
2024-05-20 20:22:22 +0500

    updated text

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       to apply for a work visa (for stays exceeding 90 days) Continue
       Appointment waiting list to apply for a work visa - Only for applicants
       with pre-approval as per §81a Residence Act Residence Act Continue
-      Appointment waiting list for Schengen visa Continue 
+      Appointment waiting list for Schengen visa Continue Study
       <select id='appointment_newAppointmentForm_fields_3__content'>
         {/* add options */}
         <option value='1'>1</option>
